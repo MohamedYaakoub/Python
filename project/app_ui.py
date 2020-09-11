06c336993d96230b7435132218fd89e386f25d40
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-11 20:29:35 +0200

    - Creates example first page of a Hyree - Creates script.js file - Creates job_requests.csv file

--- project/app_ui.py
+++ project/app_ui.py
@@ -3,10 +3,35 @@ User Interface
 """
 
 import eel
+import pandas as pd
+
+
+class JobRequests:
+    """" This class handles the data from the job_requests database.
+
+        Args:
+        file (str): Path of the csv file with the job requests
+
+        Attributes:
+        df (str): This is where the data from the csv file is stored """
+
+    def __init__(self, file):
+        self.df = pd.read_csv(file)
+        self.available = self.df[self.df['status'] == "available"]
+
 
 def main():
     eel.init('front_end')
-    eel.start('main.html')
+
+    jobs = JobRequests("simulation/job_requests.csv")
+    for index, row in jobs.available.iterrows():
+        eel.post_job(row['company'],row['title'],row['description'],row['id'])
+
+
+    eel.start('main.html', size=(800, 1200) )
+
+
 
 if __name__ == "__main__":
     main()
+
---
+++ project/front_end/script.js
@@ -0,0 +1,21 @@
+eel.expose(post_job);
+function post_job(company,title,description,id) {
+    var para = document.createElement("P");                 // Create a <p> element
+    para.innerHTML = "<div class=\"card\" id=\"idn"+id+"\" style=\"width: 100%;\">\n" +
+        "  <div class=\"card-body\">\n" +
+        "    <h5 class=\"card-title\">" + company + "</h5>\n" +
+        "    <h6 class=\"card-subtitle mb-2 text-muted\">"+title+ "</h6>\n" +
+        "    <p class=\"card-text\">"+description+"</p>\n" +
+        "    <a href=\"#\" onclick=\"acceptJob("+id+")\"class=\"card-link\">Accept</a>\n" +
+        "    <a href=\"#\" class=\"card-link\">Reject</a>\n" +
+        "  </div>\n" +
+        "</div>";                // Insert text
+    document.getElementById("JobsOffer").appendChild(para);
+}
+
+
+function acceptJob(id) {
+    var divid = "idn"+ id;
+    document.getElementById(divid).style.display="none";
+
+}
