17ee9e3098a467b74180900ec7a707311f160081
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-13 17:36:39 +0200

    Clean files
    - Removes not wanted comments
    - Removes extra empty lines
    - Corrects indentation

--- project/front_end/script.js
+++ project/front_end/script.js
@@ -1,7 +1,6 @@
 // Functions related to the jobs displayed in the home page
 
 eel.expose(post_job)
-
 function post_job(company, title, description, id) {
     let para = document.createElement("P");                 // Create a <p> element
     para.innerHTML = "<div class=\"card\" id=\"idn" + id + "\" style=\"width: 100%;\">\n" +
@@ -47,25 +46,19 @@ function login() {
 
 }
 
-eel.expose(login_accepted);
-
-function login_accepted() {
-    window.location.replace('/main.html');
-}
-
 eel.expose(login_rejected)
-
 function login_rejected() {
-    // var element = document.getElementById("alert-rejected-login");
-    // element.classList.add("show");
+
     $('#alert-rejected-login').addClass("show")
     setTimeout(function () {
         $('#alert-rejected-login').removeClass("show");
     }, 4000);
 }
 
-// $(document).ready(function() {
-//   // $('.alert').hide()
-//     alert("hi")
-//     })
+eel.expose(login_accepted);
+function login_accepted() {
+    window.location.replace('/main.html');
+}
+
+
 
