becb9d9623076892de879b3e0b2b3109443380b0
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-13 23:53:38 +0200

    Change app height from 1200 to 1050 px

--- project/app_ui.py
+++ project/app_ui.py
@@ -64,7 +64,7 @@ def main():
     """ Function that initiates the app's UI by using the eel package
         """
     eel.init('front_end')
-    eel.start('home.html', size=(800, 1200))
+    eel.start('home.html', size=(800, 1050))
 
 
 if __name__ == "__main__":
