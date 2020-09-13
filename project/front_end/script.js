3d7530756626359e71cd6347787d0987d6187931
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-13 13:02:48 +0200

    Clean javascript file

--- project/front_end/script.js
+++ project/front_end/script.js
@@ -1,14 +1,14 @@
 eel.expose(post_job);
 
 function post_job(company, title, description, id) {
-    var para = document.createElement("P");                 // Create a <p> element
+    let para = document.createElement("P");                 // Create a <p> element
     para.innerHTML = "<div class=\"card\" id=\"idn" + id + "\" style=\"width: 100%;\">\n" +
         "  <div class=\"card-body\">\n" +
         "    <h5 class=\"card-title\">" + company + "</h5>\n" +
         "    <h6 class=\"card-subtitle mb-2 text-muted\">" + title + "</h6>\n" +
         "    <p class=\"card-text\">" + description + "</p>\n" +
         "    <a href=\"#\" onclick=\"acceptJob(" + id + ")\" class=\"card-link\">Accept</a>\n" +
-        "    <a href=\"#\" onclick=\"rejectJobCheck(" + id +",'"+ title +"','"+ company + "')\" class=\"card-link\">Reject</a>\n" +
+        "    <a href=\"#\" onclick=\"rejectJobCheck(" + id + ",'" + title + "','" + company + "')\" class=\"card-link\">Reject</a>\n" +
         "  </div>\n" +
         "</div>";                // Insert text
     document.getElementById("JobsOffer").appendChild(para);
@@ -16,26 +16,21 @@ function post_job(company, title, description, id) {
 
 
 function acceptJob(id) {
-    var divid = "idn" + id;
+    const divid = "idn" + id;
     document.getElementById(divid).style.display = "none";
 
 }
 
-function rejectJobCheck(id, title,company) {
-    // alert(id,title,company);
-    // $('#ModalRejectJob').collapse({
-    //   toggle: false
-    // })
-    //
+function rejectJobCheck(id, title, company) {
     $('#ModalRejectJobTitle').text('Reject offer by ' + company)
-    $('#ModalRejectJobBody').html('Are you sure you want to reject the <b>'+title+ '</b> job at <b>' + company +'</b> ?')
+    $('#ModalRejectJobBody').html('Are you sure you want to reject the <b>' + title + '</b> job at <b>' + company + '</b> ?')
     $('#ModalRejectJob').modal('show')
-    $("#modalConfirmRejectbtn").attr("onclick","rejectJob("+id+")");
+    $("#modalConfirmRejectbtn").attr("onclick", "rejectJob(" + id + ")");
 
 }
 
 function rejectJob(id) {
-    var divid = "idn" + id;
+    const divid = "idn" + id;
     $('#ModalRejectJob').modal('hide')
     document.getElementById(divid).style.display = "none";
 
@@ -43,8 +38,8 @@ function rejectJob(id) {
 
 
 function login() {
-    var email = document.getElementById('LoginEmail').value;
-    var password = document.getElementById('LoginPassword').value;
+    const email = document.getElementById('LoginEmail').value;
+    const password = document.getElementById('LoginPassword').value;
     eel.log_in(email, password);
 
 }
