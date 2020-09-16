a4f93f5c54c22e93e28d5d459b75013fd0d979d6
Jasper Jeuken <j.jeuken@student.tudelft.nl>
2020-09-16 16:24:58 +0200

    Conform Python file names to snake_case style

--- project/hyree_main.py
+++ project/hyree_main.py
@@ -3,20 +3,15 @@ User Interface
 """
 
 import eel
-from hyreeLogin import *
-from hyreeUpdateProfile import *
-from hyreeDashboard import *
-
-
-
-
+from hyree_login import *
+from hyree_update_profile import *
+from hyree_dashboard import *
 
 def main():
     """ Function that initiates the app's UI by using the eel package
         """
     eel.init('front_end')
     eel.start('home.html', size=(800, 1050))
-
 
 if __name__ == "__main__":
     main()
