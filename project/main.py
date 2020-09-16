1b5857edf0f4415f33070bfd8178bdd9bb018813
Jasper Jeuken <j.jeuken@student.tudelft.nl>
2020-09-16 17:27:17 +0200

    More improvements to importing in main.py

--- project/main.py
+++ project/main.py
@@ -3,9 +3,9 @@ User Interface
 """
 
 import eel
-from hyree_login import log_in, log_out # pylint: disable=unused-import
-from hyree_update_profile import get_user_information, update_user_information # pylint: disable=unused-import
-from hyree_dashboard import JobRequests, write_new_jobs, write_old_jobs, write_jobs # pylint: disable=unused-import
+from hyree_login import log_in, log_out # pylint: disable=unused-import, disable=import-error
+from hyree_update_profile import get_user_information, update_user_information # pylint: disable=unused-import, disable=import-error
+from hyree_dashboard import JobRequests, write_new_jobs, write_old_jobs, write_jobs # pylint: disable=unused-import, disable=import-error
 
 def main():
     """ Function that initiates the app's UI by using the eel package
