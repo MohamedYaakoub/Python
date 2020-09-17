feeae7e12da7be78c91260eae2c0b22972fb6b27
ZeyadFathallah <zeyadheshamfarouk@gmail.com>
2020-09-18 00:44:33 +0200

    First backend commit.

---
+++ project/backend.py
@@ -0,0 +1,94 @@
+from time import ctime
+from random import randint
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import ntplib
+
+
+class Database:
+    def __init__(self, json, sheet_name):
+        self.scope = ['https://spreadsheets.google.com/feeds',
+                      'https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name(json, self.scope)
+        self.client = gspread.authorize(self.creds)
+        self.sheet = self.client.open(sheet_name).sheet1
+        self.size = self.count()
+
+    def get_all(self):
+        return self.sheet.get_all_values()
+
+    def add(self, request):
+        # for entry in
+        self.sheet.insert_row(request, 2)
+
+    def delete(self, index):
+        self.sheet.delete_rows(index)
+
+    def count(self):
+        count = 0
+        for entry in self.get_all():
+            count += 1 if ''.join(entry) != '' else 0
+        return count
+    # TODO: 1- remove() to remove entry given ID (either first matching entry or all)
+    # TODO: 2- clean() to remove empty lines automatically
+    # TODO: 3- amend(id, new_entry) to update given line (add just one cell)
+
+
+class Request:
+    def __init__(self, request_details):
+        self.identifier = int(request_details[0])
+        self.date = request_details[1]
+        # TODO: 4- get time automatically (from internet) and add request time. User
+        self.time = request_details[2]
+        self.location = str(request_details[3])
+        self.hyrer = str(request_details[4])
+        self.category = str(request_details[5])
+        self.description = str(request_details[6])
+        self.hyree_assigned = str(request_details[7])
+        self.status = 'Active'
+
+    @staticmethod
+    def get_time():
+        """Gets global time time
+
+              :param :
+              :return: time
+            """
+        ntp_client = ntplib.NTPClient()
+        response = ntp_client.request('pool.ntp.org')  # Time zone
+        return ctime(response.tx_time)
+
+    def accept(self):
+        self.status = 'Accepted'
+
+    def reject(self):
+        self.status = 'Active'
+
+    def done(self):
+        self.status = 'Done'
+
+
+class User:
+    user_database = Database("client_secret_1.json", "User Database")
+
+    def __init__(self, name, birthdate, location, preferences):
+        self.user_id = randint(10000, 99999)
+        while str(self.user_id) in self.user_database.sheet.col_values(1):
+            self.user_id = randint(10000, 99999)
+        self.name = name
+        self.birthdate = birthdate
+        self.location = location
+        self.preferences = preferences
+
+    def add_user(self):
+        self.user_database.add([str(self.user_id), self.name,
+                                self.birthdate, self.location, self.preferences])
+
+    # @classmethod
+    # def number_users(cls):
+    #     cls.users = 5
+
+
+
+# Testing
+# if __name__ == '__main__':
