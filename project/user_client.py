bbea3c73b2ca6f7072fa8b7f014c1d261d4e44b4
Mohamed <mohsyd10@yahoo.com>
2020-09-18 02:20:50 +0200

    waiting

--- project/user_client.py
+++ project/user_client.py
@@ -3,25 +3,20 @@ from oauth2client.service_account import ServiceAccountCredentials
 import time
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret_1.json', scope)
-client = gspread.authorize(creds)
-
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-sheet = client.open("Testing").sheet1
-
-all = sheet.get_all_records()
-print(all)
 
 
-print()
-print("7amada")
+
 
 class Database:
-    def __init__(self, creds, client):
+    def __init__(self):
         scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
                  "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret_1.json', scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret_3.json', scope)
         client = gspread.authorize(creds)
+        self.sheet = client.open("Database 3").sheet1
+
+
+
+
