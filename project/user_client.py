8043fb42fc93fa129bfb48e1eb2d7d9c0bdca0e0
Mohamed <mohsyd10@yahoo.com>
2020-09-18 02:24:17 +0200

    remove everything in user client

--- project/user_client.py
+++ project/user_client.py
@@ -1,21 +1,3 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import time
-
-
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-
-
-
-
-class Database:
-    def __init__(self):
-        scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-                 "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret_3.json', scope)
-        client = gspread.authorize(creds)
-        self.sheet = client.open("Database 3").sheet1
 
 
 
