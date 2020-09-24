0bd3bcd67b47c4702de1fc41c42e6edea6e8f15c
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-24 10:56:51 +0200

    Make navbar of hyree pages stay fixed when scrolling

--- project/front_end/js/script.js
+++ project/front_end/js/script.js
@@ -49,3 +49,16 @@ function otherjobtype() {
 
 }
 
+if ($(window).width() > 100) {
+  $(window).scroll(function(){
+     if ($(this).scrollTop() > 40) {
+        $('#navbar_top').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar_top').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }
+  });
+}
