8cb828b10ce8e1e4631e36c2a21177018207b60e
Mohamed <mohsyd10@yahoo.com>
2020-09-24 20:30:35 +0200

    changed functions to allow UI implementing

--- project/hyree_client.py
+++ project/hyree_client.py
@@ -2,65 +2,67 @@ from project.backend import Database,Request,User,find
 import time
 import threading
 
-def login():
-
-    where = input(str("login or new user? "))   #implement in the UI
-    database = Database("client_secret_1.json", "User Database")
-    user_database = database.get_all()
-    #usernames = user_database["Name"].tolist()
-    passwords = user_database["Password"].tolist()
-    emails = user_database["Email"].tolist()
-
-    if where == "login":
-        email = input(str("email: "))
-        pw = input(str("password: "))
-
-        if email in emails:
-            index = emails.index(email)
-
-            if pw in passwords[index]:
-                print("you are in!!")
-                current_user = find(user_database,Email=email)[1].iloc[0]       #Here you can import all attributes such as Hyree ID, email, password
-                print("your user id is " + current_user["Hyree ID"])
-
-
-                onlinelog = threading.Thread(target=online_log, args=(database,index), daemon=True)
-                onlinelog.start()
-
-                print("weeeeeeeeeeeeeeeeee")
-                time.sleep(100)
-
-            else:
-                print("Incorrect Password")     #user should try again
-
-        else:
-            print("Non existing username")      #user should try again
-
-    elif where == "new user":
-        #emails = get_userinfo()[4].tolist()             #check with earlier better method
-        email = input(str("Email: "))
-
-        while email in emails:
-            print("email already in use")
-            email = input(str("Email: "))
-
-        user = User(input(str("name: ")),
-                    input(str("Data of birth ""dd/mm/yyyy"": ")),           #give error if wrong format provided
-                    input(str("Location (where you currently live): ")),
-                    input(str("Job Prefrences: ")),                         #should be a drop down menu
-                    input(email),
-                    input(str("Password: ")))
-        user.user_id = "Hy - " + str(user.user_id)
-        user.add_user()
-        print("User successfully added.")
-        login()             #to login again
-
+def login_acc(email, password, df):
+    try:
+        user = find(df, Email=email)[1].iloc[0]
+        if user["Password"] == password:
+            print("you are in!!")
+            current_user = find(df, Email=email)[1].iloc[0]  # Here you can import all attributes such as Hyree ID, email, password
+            print("your user id is " + current_user["Hyree ID"])
+            return current_user
+    except:
+        print("Non existing email")
+        pass
+    return False, print("Incorrect Password")
+#login_acc("yoyo","amen",Database("client_secret_1.json", "User Database").get_all())
 
 def online_log(userinfo,index):
     userinfo.add_cell(index+2, 8, "Online")
     while True:       #True essentially means that the user is online
         userinfo.add_cell(index+2, 9, Request.get_time1())
         time.sleep(2)
+
+# onlinelog = threading.Thread(target=online_log, args=(database,index), daemon=True)
+# onlinelog.start()                                                                         #These should be implemented
+
+
+def email_in_use(email, df):        ##New user##    #True if the email is not in use
+    try:
+        err = find(df, Email = email)[1].iloc[0]
+        return False, print("Email already in use")
+    except:
+        return True , email
+
+
+# if True move on to newuser
+# if email_in_use("hehe",Database("client_secret_1.json", "User Database").get_all()) is True:
+
+
+def newuser(name, email, D_O_B, Location, Prefrence, Password, df):
+    user = User(name,
+                D_O_B,  # give error if wrong format provided
+                Location,
+                Prefrence,  # should be a drop down menu
+                email,
+                Password)
+    user.user_id = "Hy - " + str(user.user_id)
+    user.add_user()
+    print("User successfully added.")
+
+def edit_user(user, name, email, D_O_B, Location, Prefrence, Password):
+    user["Name"] = name
+    user["Email"] = email
+    user["Password"] = D_O_B
+    user["Name"] = Location
+    user["Email"] = Prefrence
+    user["Password"] = Password
+    #update name email password
+
+def login():
+
+    # where = input(str("login or new user? "))   #implement in the UI
+    # database = Database("client_secret_1.json", "User Database")
+    # user_database = database.get_all()
 
 
 #online_log()
