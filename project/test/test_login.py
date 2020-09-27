fb4df873fa6dff07e73932b8fae94e76b6832f96
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-27 17:49:39 +0200

    Add a test (test not working yet)

--- project/test/test_login.py
+++ project/test/test_login.py
@@ -3,8 +3,16 @@ Created on 27/09/2020
 
 @author: Lucas V. dos Santos
 """
-
+from project.backend import Database
+from project.login import login_acc
 import unittest
-from ... import login
+import os.path
 
-print(log_in(1,2))
+class testLogin(unittest.TestCase):
+
+    # def setUp(self):
+    #     self.hyree = Database("../client_secret_1.json", "User Database").get_all()
+
+    def test_hyree_log_in(self):
+        self.assertEqual(login_acc('user@hyrer.com','lucas',  Database("../client_secret_1.json", "User Database").get_all()), True)
+        # self.assertEqual(login_acc('user@hyrer.com', '123',  Database("../client_secret_1.json", "User Database").get_all()), False)
