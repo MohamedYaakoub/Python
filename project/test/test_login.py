11c25a468a90386be8ebd1bb234cc3db307e428e
Lucas dos Santos <lucas6eng@gmail.com>
2020-09-28 10:23:20 +0200

    Add hyree login tests

--- project/test/test_login.py
+++ project/test/test_login.py
@@ -3,16 +3,18 @@ Created on 27/09/2020
 
 @author: Lucas V. dos Santos
 """
+
+
 from project.backend import Database
 from project.login import login_acc
 import unittest
-import os.path
 
 class testLogin(unittest.TestCase):
 
-    # def setUp(self):
-    #     self.hyree = Database("../client_secret_1.json", "User Database").get_all()
+    def setUp(self):
+        self.hyree = Database("../client_secret_1.json", "User Database").get_all()
 
     def test_hyree_log_in(self):
-        self.assertEqual(login_acc('user@hyrer.com','lucas',  Database("../client_secret_1.json", "User Database").get_all()), True)
-        # self.assertEqual(login_acc('user@hyrer.com', '123',  Database("../client_secret_1.json", "User Database").get_all()), False)
+        self.assertEqual(login_acc('user@hyre.com','lucas',  self.hyree), True)
+        self.assertEqual(login_acc('user@hyre.com', '123', self.hyree), False)
+
